4418d0605b6c8e658ff6223b1dddbbbaec3a476d
Jan Carbonell <jcllobet@gmail.com>
2024-03-19 22:10:48 -0400

    homepage checkpoint

---
+++ chat_pdf/src/app/components/button.tsx
@@ -0,0 +1,65 @@
+import clsx from "clsx";
+import Link from "next/link";
+
+const variantStyles = {
+  primary:
+    "bg-indigo-400 text-white hover:bg-indigo-300 ring-1 ring-inset ring-indigo-300",
+  secondary:
+    "bg-zinc-800 text-zinc-400 hover:bg-zinc-700 ring-1 ring-inset ring-zinc-800",
+  filled: "bg-indigo-600 text-white hover:bg-opacity-90",
+  outline:
+    "text-zinc-400 ring-1 ring-inset ring-white/10 hover:bg-white/5 hover:text-white",
+  danger: "bg-red-600 text-white hover:bg-red-500",
+  text: "text-indigo-400 hover:text-indigo-500",
+  disabled: "bg-zinc-600 text-white cursor-not-allowed hover:bg-zinc-500",
+};
+
+type ButtonProps = {
+  variant?: keyof typeof variantStyles;
+  className?: string;
+  href?: string;
+  disabled?: boolean;
+  size?: "sm" | "md" | "lg"; // Add size prop
+} & React.ComponentPropsWithoutRef<"button">;
+
+export function Button({
+  variant = "primary",
+  className,
+  children,
+  href,
+  disabled = false,
+  size = "md", // Default size
+  ...props
+}: ButtonProps) {
+  const sizeStyles = {
+    sm: "px-2 py-1 text-sm",
+    md: "px-4 py-2 text-base", // Adjust these styles as needed
+    lg: "px-6 py-3 text-lg",
+  };
+
+  className = clsx(
+    "rounded-md inline-flex gap-0.5 justify-center overflow-hidden font-medium transition",
+    variantStyles[variant],
+    sizeStyles[size], // Apply size styles
+    className
+  );
+
+  // Render a link with a button inside if href is provided
+  if (href && !disabled) {
+    // Check disabled state
+    return (
+      <Link href={href}>
+        <button className={className} {...props}>
+          {children}
+        </button>
+      </Link>
+    );
+  }
+
+  // Render a button if no href is provided
+  return (
+    <button className={className} disabled={disabled} {...props}>
+      {children}
+    </button>
+  );
+}
---
+++ chat_pdf/src/app/components/mainUploadpdf.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import {
+  FiUpload,
+  FiCheckCircle,
+  FiTrash2,
+  FiMessageCircle,
+} from "react-icons/fi";
+import Image from "next/image";
+import { Button } from "@/components/button";
+import usePdfUpload from "@/hooks/usePdfUpload";
+
+export default function DocumentUpload({
+  onChatRedirect,
+}: {
+  onChatRedirect: (chatId: string) => void;
+}) {
+  const methods = useFormContext();
+  const { control } = methods;
+
+  const { uploadStatus, pdfs, handlePdfUpload, handleRemovePdf } =
+    usePdfUpload(onChatRedirect);
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Controller
+        name="pdfs"
+        control={control}
+        render={({ field: { value } }) => (
+          <label
+            htmlFor="pdf-upload"
+            className="flex items-center justify-center gap-4 border-2 border-dashed bg-zinc-800 border-indigo-300 rounded-lg cursor-pointer hover:border-indigo-500 w-full px-4 py-6"
+          >
+            {uploadStatus === "idle" ? (
+              <div className="flex items-center text-zinc-300">
+                <FiUpload />
+                <span className="pl-3">Click to upload PDF</span>
+              </div>
+            ) : (
+              <div className="flex items-center text-green-500">
+                <FiCheckCircle />
+                <span className="pl-3">Upload Successful!</span>
+              </div>
+            )}
+            <input
+              accept="application/pdf"
+              className="hidden"
+              id="pdf-upload"
+              type="file"
+              onChange={(e) => {
+                const file = e.target.files && e.target.files[0];
+                if (file) handlePdfUpload(file);
+              }}
+            />
+          </label>
+        )}
+      />
+      {pdfs.map((pdf) => (
+        <div
+          key={pdf.id}
+          className="flex items-center gap-12 justify-between w-full p-4 border rounded-lg mt-4"
+        >
+          <div className="flex items-center">
+            <Image src="/pdf-icon.png" alt="PDF" width={24} height={24} />
+            <span className="flex-1 ml-4 text-zinc-300">{pdf.name}</span>
+            <button
+              onClick={() => handleRemovePdf(pdf.id)}
+              className="p-2 text-red-500 hover:text-red-300 rounded"
+            >
+              <FiTrash2 />
+            </button>
+          </div>
+          <div className="flex">
+            <Button
+              variant="filled"
+              className="flex items-center gap-2 p-2 rounded"
+              onClick={() => onChatRedirect(pdf.chatId)}
+            >
+              <FiMessageCircle />
+              <span>Chat with PDF</span>
+            </Button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
